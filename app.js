e722029b366e692bda925b0d300183fe2536ebbf
Amir Mustafa <amirengg15@gmail.com>
2019-03-17 18:38:37 +0500

    Added jwt authorization ApI files

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
+// Added express and jwt auth modules
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
+// Added invoked express 
 const app = express();
 
+// basic api to check response
 app.get('/api', (req, res) => {
     res.json({
         message: 'Welcome to the API'
     });
 });
 
+// generate token of the user - at present added hardcoded user
+app.post('/api/login', (req, res) => {
+    // Test User
+    const user = {
+        id: 1,
+        username: 'amir',
+        email: 'amirengg15@gmail.com'
+    }
+
+    // passing data to jwt to generate token
+    jwt.sign({user}, 'secretkey', {expiresIn: '160s'}, (err, token) => {    // expiry of token set to 160 seconds
+        res.json ({
+            token
+        })
+    })
+});
+
 app.post('/api/posts', verifyToken, (req, res) => {
     jwt.verify(req.token, 'secretkey', (err, authData) => {
         if(err) {
-            res.status(403).send('You dont have permission to access this..');
+            // res.sendStatus(403);
+            res.status(403).send('token expired');
         } else {
             res.json({
                 message: 'Post created...',
@@ -21,39 +42,18 @@ app.post('/api/posts', verifyToken, (req, res) => {
         }
     });
 });
-
-app.post('/api/login', (req, res) => {
-    const user = {
-        id: 1,
-        username: 'aamir',
-        email: 'amirengg15@gmail.com '
-    }
-
-    jwt.sign({user}, 'secretkey', { expiresIn: '160s' }, (err, token) => {
-        res.json({
-            token
-        });
-    });
-});
-// Verify Token
-// Verify Token
 function verifyToken(req, res, next) {
-  // Get auth header value
-  const bearerHeader = req.headers['authorization'];
-  // Check if bearer is undefined
-  if(typeof bearerHeader !== 'undefined') {
-    // Split at the space
-    const bearer = bearerHeader.split(' ');
-    // Get token from array
-    const bearerToken = bearer[1];
-    // Set the token
-    req.token = bearerToken;
-    // Next middleware
-    next();
-  } else {
-    // Forbidden
-    res.sendStatus(403);
-  }
-
+    const bearerHeader = req.headers['authorization'];
+    if(typeof bearerHeader != 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        // Forbidden
+        res.status(403).send('Data is forbidden');
+        // res.sendStatus(403);
+    }
 }
-app.listen(5000, () => console.log('Server started on port 5000'));
+port = 5000;
+app.listen(port, () => console.log('Listening on port 5000'));
